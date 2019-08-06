a3bd13405360fe5a73819101673d6549e37c606a
Rafael Zinezi Araújo <rafael.araujo.icsp@gmail.com>
2019-08-06 20:51:46 -0300

    pedido_form

--- ERP/pedidos/urls.py
+++ ERP/pedidos/urls.py
@@ -4,5 +4,5 @@ from ERP.pedidos import views as v
 app_name = 'pedidos'
 
 urlpatterns = [
-    path('add/', v.pedido_add, name='pedido_add'),
+    path('add/', v.PedidoCreate.as_view(), name='pedido_add'),
     ]
