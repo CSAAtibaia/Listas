756a4b82a9cb637ad502890b19cd717247c8d2fb
rafaelzinezi <rafael.araujo.icsp@gmail.com>
2019-08-06 17:59:43 +0000

    restrições item/lista item/pedido 2

--- ERP/listas/views.py
+++ ERP/listas/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from .models import Lista, ItemLista
+from ERP.pedidos.models import Pedido
 # Create your views here.
 
 def lista_itens(request):
     template_name='lista_ativa.html'
     ativa_id = Lista.objects.get(ativa=True).id
-    objetcs = ItemLista.objects.filter(lista_id = ativa_id)
+    ativa_tb = ItemLista.objects.filter(lista_id = ativa_id)
+    pedidos = Pedido.objects.filter(lista_id = ativa_id)
 
-    context={'object_list': objetcs}
+
+    context={'ativa_tb': ativa_tb,
+                'pedidos': pedidos,
+                }
     return render (request, template_name, context)
