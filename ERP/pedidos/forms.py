70973af19fe6c08f8438c47fbe0b62a0437b7da2
rafaelzinezi <rafael.araujo.icsp@gmail.com>
2019-08-14 16:26:50 +0000

    pedido_form

--- ERP/pedidos/forms.py
+++ ERP/pedidos/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Pedido #, PedidoItem
+from .models import Pedido, PedidoItem
 
 class PedidoForm(forms.ModelForm):
 
     class Meta:
         model = Pedido
+        fields = '__all__'
+
+class PedidoItemForm(forms.ModelForm):
+
+    class Meta:
+        model = PedidoItem
         fields = '__all__'
--- ERP/pedidos/urls.py
+++ ERP/pedidos/urls.py
@@ -4,5 +4,6 @@ from ERP.pedidos import views as v
 app_name = 'pedidos'
 
 urlpatterns = [
-    path('add/', v.PedidoCreate.as_view(), name='pedido_add'),
+    path('add/', v.pedido_add, name='pedido_add'),
+    #path('add/', v.PedidoCreate.as_view(), name='pedido_add'),
     ]
