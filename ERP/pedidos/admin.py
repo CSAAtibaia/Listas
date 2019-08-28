ce73fdf3ffb837704d30674e8e048ce1ab47369b
rafaelzinezi <rafael.araujo.icsp@gmail.com>
2019-08-28 20:09:19 +0000

    admin change_form

--- ERP/pedidos/admin.py
+++ ERP/pedidos/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from ERP.listas.models import Lista
 from ERP.pedidos.models import Pedido, PedidoItem
 
 # Register your models here.
@@ -7,8 +8,19 @@ class PedidoItemInline(admin.TabularInline):
     model = PedidoItem
 
 class PedidoAdmin(admin.ModelAdmin):
-    fields = (('user', 'lista', 'retira'),)
     inlines = [PedidoItemInline,
         ]
 
-admin.site.register(Pedido, PedidoAdmin)
+class PedidoUser(admin.ModelAdmin):
+    inlines = [PedidoItemInline,
+        ]
+    exclude = ('user', 'lista',)
+    def save_model(self, request, obj, form, change):
+        if not change:
+            # the object is being created, so set the user
+            obj.user = request.user
+            obj.lista = Lista.objects.get(ativa=True)
+        obj.save()
+
+#admin.site.register(Pedido, PedidoAdmin)
+admin.site.register(Pedido, PedidoUser)
