c11b508e3cba1f021e3223ec31c19c3e34ed7acf
rafaelzinezi <rafael.araujo.icsp@gmail.com>
2019-10-06 19:14:00 +0000

    nav

--- ERP/urls.py
+++ ERP/urls.py
@@ -22,12 +22,12 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
     path(
-        'admin/password_reset/',
+        'password_reset/',
         auth_views.PasswordResetView.as_view(),
-        name='admin_password_reset',
+        name='password_reset',
     ),
     path(
-        'admin/password_reset/done/',
+        'password_reset/done/',
         auth_views.PasswordResetDoneView.as_view(),
         name='password_reset_done',
     ),
