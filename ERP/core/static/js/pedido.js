22bb64e805b3ca5337ae3e41f12b1ee6fa223817
rafaelzinezi <rafael.araujo.icsp@gmail.com>
2019-11-06 18:39:47 +0000

    js

--- ERP/core/static/js/pedido.js
+++ ERP/core/static/js/pedido.js
@@ -1,81 +1,40 @@
-$(document).ready(function() {
-});
-
-$('#add-item').click(function(ev) {
-  ev.preventDefault();
-  var count = $('#estoque').children().length;
-  var tmplMarkup = $('#item-estoque').html();
-  var compiledTmpl = tmplMarkup.replace(/__prefix__/g, count);
-  $('div#estoque').append(compiledTmpl);
-
-  // update form count
-  $('#id_estoque-TOTAL_FORMS').attr('value', count + 1);
-
-  // Desabilita o campo 'saldo'
-  $('#id_estoque-' + (count) + '-saldo').prop('type', 'hidden')
-
-  // some animate to scroll to view our new form
-  $('html, body').animate({
-    scrollTop: $("#add-item").position().top - 200
-  }, 800);
-
-  $('#id_estoque-' + (count) + '-produto').addClass('clProduto');
-  $('#id_estoque-' + (count) + '-quantidade').addClass('clQuantidade');
-
-  // Cria um span para mostrar o saldo na tela.
-  $('label[for="id_estoque-' + (count) + '-saldo"]').append('<span id="id_estoque-' + (count) + '-saldo-span" class="lead" style="padding-left:10px"></span>')
-  // Cria um campo com o estoque inicial.
-  $('label[for="id_estoque-' + (count) + '-saldo"]').append('<input id="id_estoque-' + (count) + '-inicial" class="form-control" type="hidden" />')
-  // Select2
-  $('.clProduto').select2()
-});
-
 let estoque
 let saldo
-let campo
-let camp2
+let campo_saldo
 let quantidade
+let campo_prod
+let pk
+let url
 
-$(document).on('change', '.clProduto', function() {
-  let self = $(this)
-  let pk = $(this).val()
-  let url = '/produto/' + pk + '/json/'
+$(document).on('change', '.clQuantidade', function() {
+  quantidade = $(this).val();
+  // Aqui é feito o cálculo de subtração do estoque
+  // saldo = Number(estoque) - Number(quantidade);
+  campo_saldo = $(this).attr('id').replace('quantidade', 'saldo')
+    saldo = 0
+  campo_prod = $(this).attr('id').replace('quantidade', 'produto')
+  pk = $('#'+campo_prod).val()
+  url = '/produto/' + pk + '/json/'
 
   $.ajax({
     url: url,
     type: 'GET',
     success: function(response) {
       estoque = response.data[0].estoque
-      campo = self.attr('id').replace('produto', 'quantidade')
-      estoque_inicial = self.attr('id').replace('produto', 'inicial')
-      // Estoque inicial.
-      $('#'+estoque_inicial).val(estoque)
-      // Remove o valor do campo 'quantidade'
-      $('#'+campo).val('')
     },
     error: function(xhr) {
       // body...
     }
   })
-});
 
-$(document).on('change', '.clQuantidade', function() {
-  quantidade = $(this).val();
-  // Aqui é feito o cálculo de subtração do estoque
-  // saldo = Number(estoque) - Number(quantidade);
-  campo = $(this).attr('id').replace('quantidade', 'saldo')
-  campo_estoque_inicial = $(this).attr('id').replace('quantidade', 'inicial')
-  estoque_inicial = $('#'+campo_estoque_inicial).val()
-  saldo = Number(estoque_inicial) - Number(quantidade)
+  saldo = Number(estoque) - Number(quantidade)
   if (saldo < 0) {
     alert('O saldo não pode ser negativo.')
     // Atribui o saldo ao campo 'saldo'
-    $('#'+campo).val('')
+    $(this).val(0)
+    $('#'+campo_saldo).val(estoque)
     return
   }
   // Atribui o saldo ao campo 'saldo'
-  $('#'+campo).val(saldo)
-  campo2 = $(this).attr('id').replace('quantidade', 'saldo-span')
-  // Atribui o saldo ao campo 'id_estoque-x-saldo-span'
-  $('#'+campo2).text(saldo)
+  $('#'+campo_saldo).val(saldo)
 });
