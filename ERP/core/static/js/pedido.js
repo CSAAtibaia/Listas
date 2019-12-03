59af8bf8a5e5adc26a9cf649222c05273804837c
rafaelzinezi <rafael.araujo.icsp@gmail.com>
2019-12-03 16:22:50 -0300

    js

--- ERP/core/static/js/pedido.js
+++ ERP/core/static/js/pedido.js
@@ -5,12 +5,39 @@ let campo_prod
 let pk
 let url
 
+$(document).on('change', '.clProduto', function() {
+  pk = $(this).val();
+  // Aqui é feito o cálculo de subtração do estoque
+  // saldo = Number(estoque) - Number(quantidade);
+  campo_saldo = $(this).attr('id').replace('produto', 'saldo')
+  campo_preco = $(this).attr('id').replace('produto', 'preco')
+  
+  url = '/produto/' + pk + '/json/'
+
+  $.ajax({
+    url: url,
+    type: 'GET',
+    success: function(response) {
+      saldo = response.data[0].saldo
+      preco = response.data[0].preco
+    },
+    error: function(xhr) {
+      // body...
+    }
+  })
+
+  $('#'+campo_saldo).val(saldo)
+  $('#'+campo_preco).val(preco)
+});
+
 $(document).on('change', '.clQuantidade', function() {
   quantidade = $(this).val();
   // Aqui é feito o cálculo de subtração do estoque
   // saldo = Number(estoque) - Number(quantidade);
   campo_saldo = $(this).attr('id').replace('quantidade', 'saldo')
   campo_prod = $(this).attr('id').replace('quantidade', 'produto')
+  campo_preco = $(this).attr('id').replace('quantidade', 'preco')
+
   pk = $('#'+campo_prod).val()
   url = '/produto/' + pk + '/json/'
 
@@ -18,18 +45,19 @@ $(document).on('change', '.clQuantidade', function() {
     url: url,
     type: 'GET',
     success: function(response) {
-      estoque = response.data[0].estoque
+      saldo = response.data[0].saldo
+      preco = response.data[0].preco
     },
     error: function(xhr) {
       // body...
     }
   })
 
-  if (Number(estoque) < Number(quantidade)) {
+  if (Number(saldo) < Number(quantidade)) {
     alert('O saldo não pode ser negativo.')
     // Atribui o saldo ao campo 'saldo'
     $(this).val(0)
-    $('#'+campo_saldo).val(estoque)
+    $('#'+campo_saldo).val(saldo)
     return
   }
   // Atribui o saldo ao campo 'saldo'
@@ -42,29 +70,29 @@ $(document).on('change', '.clQuantidade', function() {
 
   $('#add-item').click(function(ev) {
     ev.preventDefault();
-    var count = $('#estoque').children().length;
-    var tmplMarkup = $('#item-estoque').html();
+    var count = $('#item').children().length;
+    var tmplMarkup = $('#item').html();
     var compiledTmpl = tmplMarkup.replace(/__prefix__/g, count);
-    $('div#estoque').append(compiledTmpl);
+    $('div#item').append(compiledTmpl);
 
     // update form count
-    $('#id_estoque-TOTAL_FORMS').attr('value', count + 1);
+    $('#id_item-TOTAL_FORMS').attr('value', count + 1);
 
     // Desabilita o campo 'Saldo'
-    $('#id_estoque-' + (count) + '-saldo').prop('type', 'hidden')
+    $('#id_item-' + (count) + '-saldo').prop('type', 'hidden')
 
     // some animate to scroll to view our new form
     $('html, body').animate({
       scrollTop: $("#add-item").position().top - 200
     }, 800);
 
-    $('#id_estoque-' + (count) + '-produto').addClass('clProduto');
-    $('#id_estoque-' + (count) + '-quantidade').addClass('clQuantidade');
+    $('#id_item-' + (count) + '-produto').addClass('clProduto');
+    $('#id_item-' + (count) + '-quantidade').addClass('clQuantidade');
 
     // Cria um span para mostrar o saldo na tela.
-    $('label[for="id_estoque-' + (count) + '-saldo"]').append('<span id="id_estoque-' + (count) + '-saldo-span" class="lead" style="padding-left: 10px;"></span>')
+    $('label[for="id_item-' + (count) + '-saldo"]').append('<span id="id_item-' + (count) + '-saldo-span" class="lead" style="padding-left: 10px;"></span>')
     // Cria um campo com o estoque inicial.
-    $('label[for="id_estoque-' + (count) + '-saldo"]').append('<input id="id_estoque-' + (count) + '-inicial" class="form-control" type="hidden" />')
+    $('label[for="id_item-' + (count) + '-saldo"]').append('<input id="id_item-' + (count) + '-inicial" class="form-control" type="hidden" />')
     // Select2
     $('.clProduto').select2()
   });
