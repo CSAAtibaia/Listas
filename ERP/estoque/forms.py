90839a0e1f35d73697c1c6893c8c10a610259151
rafaelzinezi <rafael.araujo.icsp@gmail.com>
2019-12-10 13:12:39 +0000

    validacao saldo

--- ERP/estoque/forms.py
+++ ERP/estoque/forms.py
@@ -32,12 +32,15 @@ class PedidoItemForm(forms.ModelForm):
                 if EstoqueItens.objects.filter(produto=self.cleaned_data.get('produto'), estoque=e):
                     velho = EstoqueItens.objects.filter(produto=self.cleaned_data.get('produto'), estoque=e).first().quantidade
                     preco = Item.objects.get(produto=self.cleaned_data.get('produto')).preco
+                    saldo_item = Item.objects.get(produto=self.cleaned_data.get('produto')).saldo
                 novo = self.cleaned_data.get('quantidade')
                 saldo = e.usuario.coagri.credito - e.total
                 diferenca = novo - velho
                 #logger.error(preco)
                 if diferenca > saldo and preco < 0.01:
                     raise forms.ValidationError('Quantidade Excede o Crédito Mensal')
+                if saldo_item - diferenca < 0:
+                    raise forms.ValidationError('Saldo Negativo para o Item')
         return self.cleaned_data.get('quantidade')
 
 
@@ -45,4 +48,9 @@ class PedidoItemForm(forms.ModelForm):
         super(PedidoItemForm, self).__init__(*args, **kwargs)
         # Retorna somente produtos com estoque maior do que zero.
         itens = Item.objects.filter(saldo__gt=0)
+        try:
+            item = Item.objects.filter(pk=self.instance.produto.id)
+            itens |= item
+        except:
+            pass
         self.fields['produto'].queryset = itens
