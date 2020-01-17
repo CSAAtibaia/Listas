2aa223b96928fe6a090c7eb93259695a287cf4c9
rafaelzinezi <rafael.araujo.icsp@gmail.com>
2020-01-17 12:40:25 +0000

    ajuste email

--- ERP/estoque/email.py
+++ ERP/estoque/email.py
@@ -6,13 +6,14 @@ from django.core.mail import send_mail
 
 def email_abertura():
     lista_coagri = CoAgri.objects.filter(status='ATIVO') | CoAgri.objects.filter(status='AVISO')
-    lista_coagri = CoAgri.objects.filter(pk=7)
+    lista_coagri = lista_coagri.filter(user__is_staff=True)
+    #lista_coagri = CoAgri.objects.filter(pk=7)
     lista_emails = list(lista_coagri.values_list('user__email', flat=True))
     lista_itens = Item.objects.filter(saldo__gt=0).values_list('produto', 'saldo', 'preco')
     q = Estoque.objects.filter(aberto=True, movimento='e').aggregate(fim=Max('finaliza'))
     finaliza = q['fim']
     subject = 'Itens Disponibilizados - Finaliza em:%s' % (finaliza)
-    body = 'Prezad@s, Os itens desta semana já estão disponíveis para Pedido até dia %s.\nLista: \n' % (finaliza)
+    body = 'Prezad@s, Os itens desta semana já estão disponíveis para Pedido até dia %s.\nAcesse http://www.CSAAtibaia.in para fazer seu pedido.\n\nLista: \n' % (finaliza)
     body = body + '%-*s%s\n' % (25, 'Nome', 'Saldo')
     for item in lista_itens:
         for val in item:
@@ -30,7 +31,8 @@ def email_abertura():
 
 def email_fechamento():
     lista_coagri = CoAgri.objects.filter(status='ATIVO') | CoAgri.objects.filter(status='AVISO')
-    lista_coagri = CoAgri.objects.filter(pk=7)
+    lista_coagri = lista_coagri.filter(user__is_staff=True)
+    #lista_coagri = CoAgri.objects.filter(pk=7)
     lista_emails = list(lista_coagri.values_list('user__email', flat=True))
     subject = 'Pedidos Encerrados'
     body = 'Encerrado o período de pedidos para a próxima partilha.'
