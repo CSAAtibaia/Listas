81f8e5b4cb62cf4f942121fb67cba17eb1a882bb
Rafael <rafael.araujo.icsp@gmail.com>
2020-03-15 21:08:21 -0300

    views ajuste

--- ERP/estoque/views.py
+++ ERP/estoque/views.py
@@ -229,7 +229,7 @@ def sem_pedido(request):
         estoque__aberto=True,
         estoque__movimento='s',
         coagri__status='INATIVO',
-        coagri__status='SUSPENSO',
+        #coagri__status='SUSPENSO',
         coagri__credito=0
         ).order_by('coagri__partilha', 'coagri__higieniza', 'first_name', 'last_name', 'username')
     context = {
